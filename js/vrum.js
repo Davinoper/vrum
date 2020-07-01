fe99ed891b05436e53e6b1baadc6b2772f24cfab
Davi noleto <davidnpjs@gmail.com>
2020-06-30 21:07:12 -0300

    primeira vercao

---
+++ js/vrum.js
@@ -0,0 +1,23 @@
+
+
+function verde(){
+    var label = document.querySelector('#resultado');
+    label.textContent = "Siga";
+    document.bgColor = 'green';
+   
+}
+    
+function amarelo(){
+    var label = document.querySelector('#resultado');
+    label.textContent = "atente-se";
+    document.bgColor = 'yellow';
+
+}
+
+function vermelho(){
+    var label = document.querySelector('#resultado');
+    label.textContent = "Pare";
+    document.bgColor = 'red';
+
+}
+
